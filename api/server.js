14e1062dad982721450df2c6b620e24710ac1c39
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2018-01-10 23:51:32 +0200

    Added simple routing

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
 const port = 8000;
+const public = __dirname + '/../client/public/';
+
+app.configure = function () {
+    app.use(app.router);
+}
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(public + 'index.js'));
+});
+
+app.use('/', express.static(public));
+
+// require('../client/app/routes')(app, {});
 app.listen(port, () => {
     console.log('It`s work');
 });
