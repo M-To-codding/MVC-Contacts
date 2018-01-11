511c9e52f3ae9378d43728b90aed8ec1fe1e7641
Marina <marina.todorishina@mail.com>
2018-01-11 13:57:15 +0200

    Added view for main page

--- client/public/index.js
+++ client/public/index.js
@@ -1,3 +1,17 @@
-window.onload = function () {
-    document.body.innerHTML = 'Hello world!';
-}
+$(document).ready(function () {
+
+    function init() {
+
+        $('#main-content').html('');
+
+        $('#main-content').append('<article id="contacts-content"></article>');
+        $('#main-content').append('<article id="groups-content"></article>');
+
+        $('#contacts-content').load('contacts/contacts.html');
+        $('#groups-content').load('groups/groups.html');
+
+    }
+
+    init();
+
+});
