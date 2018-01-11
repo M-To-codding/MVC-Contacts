093ff85e16efdc3242a2eb319b78c0ed54bc889d
Marina <marina.todorishina@mail.com>
2018-01-11 13:55:48 +0200

    Fixed server, added controllers

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const app = require('express')(),
+    express = require('express'),
     bodyParser = require('body-parser'),
     config = require('./resources/config'),
-    contacts = require('./src/index');
+    main = require('./src/index'),
+    contacts = require('./src/contacts'),
+    groups = require('./src/groups');
 
 app.listen(config.port, () => {
     console.log('It`s work');
@@ -9,6 +12,10 @@ app.listen(config.port, () => {
 
 app.use(bodyParser.json());
 
-app.use('/', contacts);
+app.use('/api/v1', main);
+app.use('/api/v1', contacts);
+app.use('/api/v1', groups);
+
+app.use(express.static(__dirname + '/src/'));
 
 module.exports = app;
--- api/src/contacts.js
+++ api/src/contacts.js
@@ -1,14 +1,14 @@
 const express = require('express'),
     path = require('path'),
     router = express.Router(),
-    public = __dirname + '../../../client/public/';
+    public = __dirname + '../../../../client/public/contacts/';
 
 router.use(express.static(public));
 
-router.get('/', function (req, res, next) {
+router.get('/api/v1/contacts/', function (req, res, next) {
     res.setHeader('content-type', 'application/javascript');
-    res.sendFile(path.join(public + 'index.html'));
-    next();
+    // res.sendFile(path.join(public + 'contacts.html'));
+    // next();
 });
 
 module.exports = router;
