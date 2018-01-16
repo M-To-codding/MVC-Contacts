98a4e69b8e082e301703ad44f48aed7c5c0a198b
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2018-01-16 14:53:58 +0200

    Added displaying main page, get items from db

--- client/public/index.js
+++ client/public/index.js
@@ -1,24 +1,68 @@
-const express = require('express');
-
 $(document).ready(function () {
 
-    var url = window.location.pathname;
-
-    function init() {
-
-        if (url === '/api/v1/') {
-
-            $('#main-content').html('');
-
-            $('#main-content').append('<article id="contacts-content"></article>');
-            $('#main-content').append('<article id="groups-content"></article>');
-
-            $('#contacts-content').load('views/contacts.html');
-            $('#groups-content').load('views/groups.html');
-
-        }
+    function resetContent() {
+        $('#main-content .loading').hide();
     }
 
-    init();
+    function contactsPattern(data) {
+
+        $('#main-content').append('<div id="contacts-content" class="contacts-container"></div>');
+
+        $('#contacts-content').load('views/contacts.html', function () {
+            $(data.contacts).each(function (key, value) {
+
+                if (value.tel === undefined) {
+                    value.tel = 'Number is not specified';
+                }
+
+                let contactsData = `<li class="item contact-item">
+            <span class="contact-name">${value.name}</span>
+            <span class="contact-tel">${value.tel}</span>
+            </li>`;
+
+                $('#contacts-list').append(contactsData);
+            });
+        })
+    }
+
+    function groupsPattern(data) {
+        $('#main-content').append('<div id="groups-content" class="groups-container"></div>');
+
+        $('#groups-content').load('views/groups.html', function () {
+            $(data.groups).each(function (key, value) {
+
+                let groupsData = `<li class="item group-item">
+            <span class="group-name">${value.name}</span>
+            </li>`;
+
+                $('#groups-list').append(groupsData);
+            })
+        });
+    }
+
+    function dataRenderer() {
+
+        this.render = function () {
+            $.ajax({
+                url: '/api/v1/contacts',
+                success: function (contacts) {
+                    contactsPattern(contacts);
+                    $.ajax({
+                        url: '/api/v1/groups',
+                        success: function (groups) {
+                            groupsPattern(groups);
+                        }
+                    });
+                }
+            });
+        }
+
+    }
+
+    resetContent();
+
+    let renderer = new dataRenderer();
+    renderer.render();
+    console.log(renderer);
 
 });
