9ea6e744ba6da1875d1b8da8d26922763cd7504d
MarinaTod <marynat@softwareplanet.uk.com>
2018-01-18 02:24:49 +0200

    Created form for add groups and contacts. Added ajax data hendlers

--- client/public/views/dataHandlers/dataRenderer.js
+++ client/public/views/dataHandlers/dataRenderer.js
@@ -5,17 +5,66 @@ function resetContent() {
 function dataRenderer() {
 
     this.render = function () {
-        $.ajax({
-            url: '/api/v1/contacts',
-            success: function (contacts) {
-                contactsPattern(contacts);
-                $.ajax({
-                    url: '/api/v1/groups',
-                    success: function (groups) {
-                        groupsPattern(groups);
-                    }
-                });
-            }
-        });
+        getAllContacts();
+        getAllGroups();
     }
+}
+
+function getAllContacts() {
+    $.ajax({
+        url: '/api/v1/contacts',
+        success: function (contacts) {
+            contactsPattern(contacts);
+        }
+    });
+}
+
+function getAllGroups() {
+    $.ajax({
+        url: '/api/v1/groups',
+        success: function (groups) {
+            groupsPattern(groups);
+        }
+    });
+}
+
+function addContact() {
+    let nameInput = $('.contactName').val(),
+        telInput = $('.contactTel').val();
+
+    let item = {
+        name: nameInput,
+        tel: telInput
+    }
+    console.log(nameInput);
+    $.ajax({
+        url: '/api/v1/contacts/add',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(item),
+        dataType: 'json',
+        success: function () {
+            getAllContacts();
+        }
+    })
+}
+
+function addGroup() {
+    let nameInput = $('.groupName').val();
+
+    let item = {
+        name: nameInput,
+    }
+
+    console.log(nameInput);
+    $.ajax({
+        url: '/api/v1/groups/add',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(item),
+        dataType: 'json',
+        success: function () {
+            getAllGroups();
+        }
+    })
 }
---
+++ client/public/views/patterns/modalWindow.js
@@ -0,0 +1,49 @@
+function contactsModal() {
+    let pattern = `
+        <form id="contacts-modal" class="modal-window">
+            <input type="text" class="contactName" placeholder="Name">
+            <input type="text" class="contactTel" placeholder="Phone">
+            <button type="submit" id="submitContact"></button>
+    </form>
+    `;
+
+    return pattern;
+}
+
+function groupsModal() {
+    let pattern = `
+    
+        <form id="groups-modal" class="modal-window">
+            <input type="text" class="groupName" placeholder="Name">
+        <button type="submit" id="submitGroup"></button>
+        </form>
+    `;
+
+    return pattern;
+}
+
+function removeModal() {
+    let modal = document.querySelector('.modal-window');
+    if(modal){
+        $(modal).remove();
+    }
+}
+
+function generateModal(itemType) {
+    let contactsModalW = contactsModal(),
+        groupsModalW = groupsModal();
+
+    if (itemType === 'contacts') {
+        $('body').append(contactsModalW);
+        $('#submitContact').on('click', function () {
+            addContact();
+            removeModal();
+        })
+    } else {
+        $('body').append(groupsModalW);
+        $('#submitGroup').on('click', function () {
+            addGroup();
+            removeModal();
+        })
+    }
+}
