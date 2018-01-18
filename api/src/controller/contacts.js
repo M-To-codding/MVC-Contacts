017dc2bc6d9add73e9d940fd03987deb122cd427
MarinaTod <marynat@softwareplanet.uk.com>
2018-01-18 02:25:26 +0200

    Added controllers for creating groups and contacts

--- api/src/controller/contacts.js
+++ api/src/controller/contacts.js
@@ -6,7 +6,16 @@ const express = require('express'),
 router.get('/contacts', function (req, res, next) {
     Contact.find({}, function (err, contacts) {
         res.json({contacts});
-    })
+    });
+    // next();
 });
+
+router.post('/contacts/add', function (req, res) {
+    let contact = new Contact(req.body);
+    contact.save((err, contactObj) => {
+        res.json({contactObj});
+    });
+    // next();
+})
 
 module.exports = router;
--- api/src/controller/groups.js
+++ api/src/controller/groups.js
@@ -2,11 +2,17 @@ const express = require('express'),
     router = express.Router(),
     Group = require('../model/groups');
 
-
 router.get('/groups', function (req, res, next) {
     Group.find({}, function (err, groups) {
         res.json({groups});
     })
+});
+
+router.post('/groups/add', function (req, res, next) {
+    let group = new Group(req.body);
+    group.save((err, groupObj) => {
+        res.json({groupObj});
+    });
 });
 
 module.exports = router;
